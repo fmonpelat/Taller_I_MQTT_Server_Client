95d57ab2d2694f5d49546aee3846acbaf950d4a7
fmonpelat <facundo.monpelat@gmail.com>
2021-10-19 16:20:45 -0300

    new server ping pong client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,49 @@
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let server_host = String::from("localhost");
+    let server_port = "3333";
+
+    match TcpStream::connect(server_host+":"+server_port) {
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port {}", server_port);
+
+            let msg = b"Ping..";
+
+            stream.write(msg).unwrap();
+            println!("Sent Ping, awaiting reply...");
+
+            let mut data = [0 as u8; 6]; // using 6 byte buffer
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    match from_utf8(&data) {
+                        Ok(r_msg) => {
+                            if r_msg == "Pong.." {
+                                println!("Pong received!");
+                                thread::sleep(time::Duration::from_millis(2000));
+                                stream.write(msg).unwrap();
+                                println!("Sent Ping, awaiting reply...");
+                            } else {
+                                println!("Unexpected reply: {}", r_msg);
+                            }
+                            true
+                        },
+                        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                    };
+                },
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Client terminated.");
 }
 
 #[cfg(test)]
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,56 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use std::str;
+
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(_size) => {
+            //  send pong if ping msg is received
+            match str::from_utf8(&data) {
+                Ok(v) => {
+                    if v == "Ping.." {
+                        println!("Ping received! v = {}\n", v);
+                        stream.write(b"Pong..").unwrap();
+                    }
+                },
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let server_address = String::from("0.0.0.0");
+    let server_port = "3333";
+    let listener = TcpListener::bind(server_address+":"+server_port).unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on port {}", server_port);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
 }
 
 #[cfg(test)]
