f035d7c07326ec5fcc37e426cd0611a53dfe4a32
Cynthia Gamarra <cgamarra@fi.uba.ar>
2021-11-06 18:21:04 -0300

    Refactor server. Need to fix

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,78 +1,16 @@
-use core::time;
-use std::io::{Read, Result, Write};
-use std::net::{Shutdown, TcpListener, TcpStream};
-use std::str::from_utf8;
-use std::{thread};
 mod logger;
+mod server;
 use crate::logger::{Logger, Logging};
-
-fn handle_client(mut stream: TcpStream) -> Result<()> {
-    let mut buff = [0_u8; 7]; // using 50 u8 buffer
-
-    Ok(while match stream.read(&mut buff) {
-        Ok(_size) => {
-            //  send pong if ping msg is received
-            match from_utf8(&buff) {
-                Ok(packet) => {
-                    match packet {
-                        "Ping..." => {
-                            println!("Ping received! \n");
-                            if let Err(e) = stream.write_all(b"Pong...") {
-                                println!("Client disconnect");
-                                return Err(e) // Send client id when write_all fails
-                            }
-                        }
-                        _ => println!("Not understood this packet: {}\n", packet),
-                    }
-                    thread::sleep(time::Duration::from_millis(2000));
-                    true
-                }
-                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            };
-            true
-        }
-        Err(_) => {
-            println!(
-                "An error occurred, terminating connection with {}",
-                stream.peer_addr().unwrap()
-            );
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {})
-}
+use crate::server::Server;
 
 fn main() {
     let file_name = "../log.txt";
     let logger = Logger::new(file_name);
 
-    let server_address = String::from("0.0.0.0");
-    let server_port = "3333";
+    let server = Server::new("0.0.0.0","3333", file_name);
+    server.connect();
 
-    let listener = TcpListener::bind(server_address + ":" + server_port).unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    logger.debug("start binding".to_string());
-    println!("Server listening on port {}", server_port);
-    for stream in listener.incoming() {
-        logger.debug("start listening to clients".to_string());
-        match stream {
-            Ok(stream) => {
-                let new_conn_msg = format!("New connection: {}",stream.peer_addr().unwrap());
-                println!("New connection: {}", new_conn_msg);
-                logger.debug(new_conn_msg);
-                thread::spawn(move || {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
+    logger.info("Server terminated.".to_string());
 }
 
 #[cfg(test)]
---
+++ server/src/server.rs
@@ -0,0 +1,97 @@
+use core::time;
+use std::error::Error;
+use std::io::{Read, Result, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str::from_utf8;
+use std::{thread};
+use crate::logger::{Logger, Logging};
+
+pub struct Server<'a> {
+	server_address: &'a str, 
+  server_port: &'a str,
+	logger: Logger,   
+  }
+
+impl<'a> Server<'a> {
+  pub fn new(server_address: &'a str, server_port: &'a str, file_source: &'a str) -> Self {
+    Server{
+      server_address: server_address,
+      server_port: server_port,
+			logger: Logger::new(file_source),
+    }
+  }
+
+	fn handle_client<'b:'a>(&self, mut stream: &'b TcpStream) -> Result<()> {
+    let mut buff = [0_u8; 7]; // using 50 u8 buffer
+
+    Ok(while match stream.read(&mut buff) {
+        Ok(_size) => {
+            //  send pong if ping msg is received
+            match from_utf8(&buff) {
+                Ok(packet) => {
+                    match packet {
+                        "Ping..." => {
+                            println!("Ping received! \n");
+                            if let Err(e) = stream.write_all(b"Pong...") {
+                                println!("Client disconnect");
+                                return Err(e) // Send client id when write_all fails
+                            }
+                        }
+                        _ => println!("Not understood this packet: {}\n", packet),
+                    }
+                    thread::sleep(time::Duration::from_millis(2000));
+                    true
+                }
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+            true
+        }
+        Err(_) => {
+            println!(
+                "An error occurred, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {})
+	}
+
+  pub fn connect(&self) -> () {
+	
+		//if let listener = TcpListener::bind(self.server_address.to_owned() + "self.server_port_port") {
+		//	Ok() =>{
+//
+		//	}
+		//	Err(e) =>{
+		//		println!("Error: {}", e);
+		//	}
+		//	
+		//} 
+    let listener = TcpListener::bind(self.server_address.to_owned() + "self.server_port_port").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    self.logger.debug("start binding".to_string());
+    println!("Server listening on port {}", self.server_port);
+    for stream in listener.incoming() {
+        self.logger.debug("start listening to clients".to_string());
+        match stream {
+            Ok(stream) => {
+                let new_conn_msg = format!("New connection: {}",stream.peer_addr().unwrap());
+                println!("New connection: {}", new_conn_msg);
+                self.logger.debug(new_conn_msg);
+                thread::spawn(move || {
+                    // connection succeeded
+                    self.handle_client(&stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
+
+  }
+}
