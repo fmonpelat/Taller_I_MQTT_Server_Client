daff33c1d765837c60cacf3f24c1cce7381430eb
fmonpelat <facundo.monpelat@gmail.com>
2021-11-07 20:23:44 -0300

    refactor iteration over parts of struct

--- server/src/mqtt_packet/payload_packet.rs
+++ server/src/mqtt_packet/payload_packet.rs
@@ -16,50 +16,20 @@ impl PacketPayload for Payload {
     fn value(&self) -> Vec<u8> {
         let mut payload_vec: Vec<u8> = Vec::with_capacity(1024);
 
-        let client_identifier_ = (self.client_identifier.len() as u16).to_be_bytes();
-        payload_vec.extend(
-            if self.client_identifier.len() > 0 {
-                client_identifier_.iter()
-            } else { [].iter() }
-        );
-        payload_vec.extend(self.client_identifier.as_bytes());
-
-
-        let will_topic_ = (self.will_topic.len() as u16).to_be_bytes();
-        payload_vec.extend(
-            if self.will_topic.len() > 0 {
-                will_topic_.iter()
-            } else { [].iter() }
-        );
-        payload_vec.extend(self.will_topic.as_bytes());
-
-
-        let will_message_ = (self.will_message.len() as u16).to_be_bytes();
-        payload_vec.extend(
-            if self.will_message.len() > 0 {
-                will_message_.iter()
-            } else { [].iter() }
-        );
-        payload_vec.extend(self.will_message.as_bytes());
-
-
-        let user_name_ = (self.user_name.len() as u16).to_be_bytes();
-        payload_vec.extend(
-            if self.user_name.len() > 0 {
-                user_name_.iter()
-            } else { [].iter() }
-        );
-        payload_vec.extend(self.user_name.as_bytes());
-
-
-        let password_ = (self.password.len() as u16).to_be_bytes();
-        payload_vec.extend(
-                if self.password.len() > 0 {
-                    password_.iter()
-                } else { [].iter() }
-        );
-        payload_vec.extend(self.password.as_bytes());
-
+        let slice: &[&dyn ::std::fmt::Display] = 
+            &[&self.client_identifier, &self.will_topic, &self.will_message, &self.user_name, &self.password];
+        let _parts: Vec<_> = slice.iter().map(
+            |x| {
+                let x_ = (x.to_string().len() as u16).to_be_bytes();
+                payload_vec.extend(
+                    if x.to_string().len() > 0 {
+                        x_.iter()
+                    } else { [].iter() }
+                );
+                payload_vec.extend(x.to_string().as_bytes());
+                x.to_string()
+            }
+        ).collect();
 
         if payload_vec.len() == 0 {
             return vec![];
