1e1a6e42fdf0d033ead756b1243d52287d5adfb4
Gaston11 <gaston@Debian9.gastonCasa>
2021-11-10 23:50:18 -0300

    fix 'fn log' resolved

--- server/src/logger.rs
+++ server/src/logger.rs
@@ -1,5 +1,5 @@
 use std::fs::{File, OpenOptions};
-use std::io::BufWriter;
+use std::io::{BufWriter, ErrorKind};
 use std::io::Write;
 use std::sync::{Arc, Mutex};
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -11,10 +11,10 @@ pub struct Logger {
 }
 pub trait Logging {
     fn new(file_source:&str) -> Logger;
-    fn log(&self, msg: String) -> Result< Error>;
-    fn debug(&self, message: String);
-    fn error(&self, message: String);
-    fn info(&self, message: String);
+    fn log(&self, msg: String) -> Result<&'static str> ;
+    fn debug(&self, message: String) -> Result<&'static str> ;
+    fn error(&self, message: String)-> Result<&'static str> ;
+    fn info(&self, message: String)-> Result<&'static str> ;
 }
 
 impl Logging for Logger{
@@ -32,7 +32,7 @@ impl Logging for Logger{
            Logger { file: Arc::new(Mutex::new(BufWriter::new(file))), _file_source: file_source.to_owned() }
     }
 
-    fn log(&self, message: String) -> Result<()> {
+    fn log(&self, message: String) -> Result<&'static str> {
         let start = SystemTime::now();
         let timestamp_str = start
             .duration_since(UNIX_EPOCH)
@@ -42,25 +42,23 @@ impl Logging for Logger{
         .lock()
         {
             Ok(mut file) => {
-                file.write_all(format!("{} {}\n", timestamp_str.as_secs(), message).as_bytes());
+                file.write_all(format!("{} {}\n", timestamp_str.as_secs(), message).as_bytes())?;
                 file.flush()?;
-                Ok(())   
-
+                Ok("return to log")   
             }
-            Err(_) => panic!("Unable to lock file"),
-        }
-        //return 
+            Err(_) => Err(Error::new(ErrorKind::Other, "Error logging")),
+        }  
     } 
     
-    fn debug(&self, message: String) {
-        self.log("[DEBUG] ".to_string() + &message);
+    fn debug(&self, message: String) -> Result<&'static str> {
+        self.log("[DEBUG] ".to_string() + &message)
     }
 
-    fn error(&self, message: String) {
-        self.log("[ERROR] ".to_string() + &message);
+    fn error(&self, message: String) -> Result<&'static str> {
+        self.log("[ERROR] ".to_string() + &message)
     }
 
-    fn info(&self, message: String) {
-        self.log("[INFO] ".to_string() + &message);
+    fn info(&self, message: String) -> Result<&'static str> {
+        self.log("[INFO] ".to_string() + &message)
     }
 }
