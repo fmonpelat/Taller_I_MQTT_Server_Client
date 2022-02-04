aee2c82ced7d18826d83933eccdcf700f07de843
Cynthia Gamarra <cgamarra@fi.uba.ar>
2022-02-04 00:34:19 -0300

    Organizing the code and change logic to connect

--- client_device/src/main.rs
+++ client_device/src/main.rs
@@ -1,5 +1,4 @@
-mod clientDevice;
-use crate::clientDevice::ClientDevice;
+use client::client::Client;
 use core::time;
 use mqtt_packet::mqtt_packet_service::payload_packet::Payload;
 use mqtt_packet::mqtt_packet_service::variable_header_packet::VariableHeader;
@@ -8,26 +7,26 @@ use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 use rand::Rng;
+use rand::distributions:: Uniform;
 
 fn main() {
-    let mut client = ClientDevice::new();
-
-    println!("MQTT Client V2.0\n");
-
-    loop {
-
-        // creating a new default mqtt packet
-        let packet: Packet<VariableHeader, Payload> = Packet::<VariableHeader, Payload>::new();
-        println!("######## Connection to localhost ########");
-        client
-            .connect(
+    let mut client = Client::new();
+    println!("######## Connection to localhost ########");
+    client.connect(
                 "localhost".to_string(),
                 "3333".to_string(),
+                "".to_string(),
+                "".to_string(),
                 true,
                 "".to_string(),
                 "".to_string(),
             )
             .expect("Error connecting");
+
+    loop {
+        // creating a new default mqtt packet
+        let packet: Packet<VariableHeader, Payload> = Packet::<VariableHeader, Payload>::new();
+        
         let client_identifier = client.get_id_client();
         println!("--> Trying to connect with client id {}", client_identifier);
         println!("Send connect");
@@ -35,8 +34,9 @@ fn main() {
         let conn_retries = client.get_connect_retries();
         loop {
             if i >= conn_retries {
-                println!(" <-- Not connected to server");
-                break;
+                // print something as error on loop
+                client.disconnect();
+                panic!("Connection to client failed.");
             }
             if client.is_connected() {
                 println!(
@@ -49,37 +49,27 @@ fn main() {
             thread::sleep(time::Duration::from_millis(1000));
             println!("Waiting for connection ... retries: {}/{}", i, conn_retries);
         }
-        if !client.is_connected() {
-            println!(
-                "Client is not connected to server. Retrying ",
-            );
-            continue;
-        }
 
         if client.is_connected() {
             // Publish to the server
             println!("######## Publishing the topic to the server ########");
             let packet_identifier = client.get_packet_identifier();
             let mut rng = rand::thread_rng();
-            let temperature = rng.gen_range(-100..100);
+            let interval = Uniform::from(1111..9999);
+            let token: i32 = rng.sample(interval);
             let packet = packet.publish(
                 0,
                 1,
                 0,
                 packet_identifier,
-                "temperature".to_string(),
-                temperature.to_string(),
+                "token".to_string(),
+                token.to_string(),
             );
             println!("Sending packet: {:?}", packet.value());
-            client.send(packet.value());
-
-            // Disconnect from the server   
-            client.disconnect();
-            println!("--> Disconnected from server.");
-
+            client.send(packet.value())
         }
-        println!("########## Client will publish in 30 seconds ########");
-        sleep(Duration::new(30, 0));       
+        println!("########## Client will publish in 20 seconds ########");
+        sleep(Duration::new(20, 0));       
     }
 }
 
